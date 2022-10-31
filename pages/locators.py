6fd84ede9834eccfe002625b428ed076af257dc9
kiwi <sergeeva.n.yu@gmail.com>
2022-11-01 02:00:08 +0300

    LoginPageLocators added

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,25 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, "div.basket-mini a.btn-default")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class BasketPageLocators:
+    EMPTY_BASKET_MSG = (By.CSS_SELECTOR, "#content_inner > p:nth-child(1)")
+    PRODUCTS_IN_BASKET_TEXT = (By.CSS_SELECTOR, "h2.col-sm-6")
+
+
 class LoginPageLocators:
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    CONFIRM_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BTN = (By.CSS_SELECTOR, '#register_form > .btn[name="registration_submit"]')
 
 
 class ProductPageLocators:
@@ -12,14 +28,3 @@ class ProductPageLocators:
     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
     ADD_TO_BASKET_SUCCESS_MSG = (By.CSS_SELECTOR, "#messages > div:nth-child(1) strong")
     BASKET_PRICE_MSG = (By.CSS_SELECTOR, "#messages > div.alert-info p strong")
-
-
-class BasePageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_LINK = (By.CSS_SELECTOR, "div.basket-mini a.btn-default")
-
-
-class BasketPageLocators:
-    EMPTY_BASKET_MSG = (By.CSS_SELECTOR, "#content_inner > p:nth-child(1)")
-    PRODUCTS_IN_BASKET_TEXT = (By.CSS_SELECTOR, "h2.col-sm-6")
