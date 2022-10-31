2e0380995c783023f43d8878f3af105db15ad8b8
kiwi <sergeeva.n.yu@gmail.com>
2022-11-01 02:20:21 +0300

    method register_new_user added

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,20 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        assert self.is_element_present(*LoginPageLocators.EMAIL_FIELD), "Email field is not presented"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_FIELD), "Password field is not presented"
+        assert self.is_element_present(
+            *LoginPageLocators.CONFIRM_PASSWORD_FIELD), "Password confirmation field is not presented"
+        email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
+        email_field.send_keys(email)
+        password_field = self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD)
+        password_field.send_keys(password)
+        password_confirm_field = self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD_FIELD)
+        password_confirm_field.send_keys(password)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BTN)
+        register_button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
